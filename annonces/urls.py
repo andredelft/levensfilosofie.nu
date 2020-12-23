f610dac66c6abd12288e056bbfaffbbdc4ae3a83
André van Delft <andrevandelft@outlook.com>
2020-12-23 22:38:00 +0100

    Style detailview and listview

--- annonces/urls.py
+++ annonces/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from annonces.views import AnnonceList, AnnonceDetail
 
 urlpatterns = [
-    path('', AnnonceList.as_view(), name='announcement_list'),
-    path('<slug:slug>', AnnonceDetail.as_view(), name='announcement_detail')
+    path('', AnnonceList.as_view(), name='annonce_list'),
+    path('<slug:slug>', AnnonceDetail.as_view(), name='annonce_detail')
 ]
