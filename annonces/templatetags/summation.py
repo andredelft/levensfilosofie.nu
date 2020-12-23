47f53507dbb450a174722c74de47d47e92024715
André van Delft <andrevandelft@outlook.com>
2020-12-23 22:54:36 +0100

    Some more styling

---
+++ annonces/templatetags/summation.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def dutch_summation(values_list):
+    string_list = [str(item) for item in values_list]
+    if len(string_list) == 1:
+        return item_list[0]
+    else:
+        return f"{', '.join(string_list[:-1])} en {string_list[-1]}"
