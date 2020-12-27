769ef170b7f347c43258932b581370280a6a7dab
André van Delft <andrevandelft@outlook.com>
2020-12-27 13:52:19 +0100

    Simplify views

--- annonces/views.py
+++ annonces/views.py
@@ -7,16 +7,17 @@ from annonces.models import Symposium
 # Create your views here.
 
 class AnnonceList(ListView):
-    queryset = Symposium.objects.all().prefetch_related('talk_set', 'programitem_set').order_by('-date')
+    model = Symposium
+    ordering = '-date'
     context_object_name = 'symposia'
 
 
 class AnnonceDetail(DetailView):
-    model = Symposium
+    queryset = Symposium.objects.prefetch_related('talk_set', 'programitem_set')
     context_object_name = 'symposium'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['program'] = self.object.programitem_set.filter(symposium=self.object).order_by('number')
-        context['talks'] = self.object.talk_set.filter(symposium=self.object).order_by('number')
+        context['program'] = self.object.programitem_set.order_by('number')
+        context['talks'] = self.object.talk_set.order_by('number')
         return context
