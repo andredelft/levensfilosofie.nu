8692962d07d1efde445f4809e26ba8097f4635c8
André van Delft <andrevandelft@outlook.com>
2021-01-04 17:04:41 +0100

    Render the upcoming symposium on the home page

--- levensfilosofie/views.py
+++ levensfilosofie/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from annonces.models import Symposium
+from datetime import date, timedelta
+
+UPCOMING_DAYS = 28
+
 def home(request):
-    return render(request, 'index.html', {'expand_header': True})
+    today = date.today()
+    upcoming_symposium = Symposium.objects.filter(date__range=(today, today + timedelta(28))).order_by('date').first()
+
+    return render(request, 'index.html', {'expand_header': True, 'upcoming_symposium': upcoming_symposium})
