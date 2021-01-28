127f3c5f9c160c69ecd19e1f27a5c89683dea8d3
André van Delft <andrevandelft@outlook.com>
2021-01-28 12:00:15 +0100

    Small cleanup

--- annonces/templatetags/markdown.py
+++ annonces/templatetags/markdown.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import SafeString
+from django.conf import settings
 
 import re
 from markdown import markdown
@@ -11,13 +12,16 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def render_markdown(content):
-    return SafeString(markdown(content, extensions=[UrlizeExtension()]))
+    return SafeString(markdown(content, extensions=[UrlizeExtension(), 'smarty']))
 
 @register.filter(is_safe=True)
 @stringfilter
 def render_markdown_line(content):
     if '\n\n' in content:
-        raise ValueError('render_markdown_line expects one block (e.g., no double newlines)')
+        if settings.DEBUG:
+            raise ValueError('render_markdown_line expects one block (e.g., no double newlines)')
+        else:
+            content = content.split('\n\n')[0]
     md = render_markdown(content)
     md = re.sub('^\s*<p>\s*|\s*</p>\s*$', '', md)
     return SafeString(md)
