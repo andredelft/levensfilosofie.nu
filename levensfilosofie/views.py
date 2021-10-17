2621a32abd5f3ed747406854d21524f5c7676819
André van Delft <andrevandelft@outlook.com>
2021-10-17 15:04:42 +0200

    Brand new landing page

--- levensfilosofie/views.py
+++ levensfilosofie/views.py
@@ -5,8 +5,16 @@ from datetime import date, timedelta
 
 UPCOMING_DAYS = 28
 
+
 def home(request):
     today = date.today()
-    upcoming_symposium = Symposium.objects.filter(date__range=(today, today + timedelta(28))).order_by('date').first()
+    upcoming_symposium = Symposium.objects.filter(
+        date__range=(today, today + timedelta(28))).order_by('date').first()
 
-    return render(request, 'index.html', {'expand_header': True, 'upcoming_symposium': upcoming_symposium})
+    return render(
+        request, 'index.html',
+        {
+            'hide_logo': True,
+            'upcoming_symposium': upcoming_symposium
+        }
+    )
