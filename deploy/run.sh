2ca2e750bd6abbebcb7ae21da808f45f98202a43
André van Delft <andrevandelft@outlook.com>
2021-10-30 00:05:02 +0200

    Change some production configs

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,2 @@
 source .env
-docker run --name ${DOCKER_CONTAINER_NAME} --env DEBUG=0 --env-file ${ENV_FILE} -p ${HOST_PORT}:8000 --detach ${DOCKER_IMAGE_TAG} -v ${DOCKER_DB_VOLUME}:/db
+docker run --name ${DOCKER_CONTAINER_NAME} --env DEBUG=0 --env-file ${ENV_FILE} -p ${HOST_PORT}:8000 --detach -v ${DOCKER_DB_VOLUME}:/db ${DOCKER_IMAGE_TAG}
