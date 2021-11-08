f36afaeee6490a46d736621ae3460581d1cbb067
André van Delft <andrevandelft@outlook.com>
2021-11-08 01:20:32 +0100

    Add agenda with upcoming symposia to homepage

--- annonces/templatetags/summation.py
+++ annonces/templatetags/summation.py
@@ -6,7 +6,9 @@ register = template.Library()
 @register.filter
 def dutch_summation(values_list):
     string_list = [str(item) for item in values_list]
-    if len(string_list) == 1:
+    if len(string_list) == 0:
+        return ""
+    elif len(string_list) == 1:
         return values_list[0]
     else:
         return f"{', '.join(string_list[:-1])} en {string_list[-1]}"
--- levensfilosofie/views.py
+++ levensfilosofie/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
 
 from annonces.models import Symposium
-from datetime import date, timedelta
+from datetime import date
 
-UPCOMING_DAYS = 28
+TODAY = date.today()
 
 
 def home(request):
-    today = date.today()
-    upcoming_symposium = (
-        Symposium.objects.filter(
-            to_be_announced=False, date__range=(today, today + timedelta(28))
-        )
-        .order_by("date")
-        .first()
+    upcoming_symposia = Symposium.objects.filter(date__gte=TODAY).order_by(
+        "date"
     )
 
     return render(
         request,
         "index.html",
-        {"hide_logo": True, "upcoming_symposium": upcoming_symposium},
+        {"hide_logo": True, "upcoming_symposia": upcoming_symposia},
     )
