e7836bd07565e383d9195450e97b7f0f7125cfa0
André van Delft <andrevandelft@outlook.com>
2023-02-05 15:10:55 +0100

    Fix programitem_set function

--- annonces/views.py
+++ annonces/views.py
@@ -18,6 +18,6 @@ class AnnonceDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["program"] = self.object.programitem_set
+        context["program"] = self.object.programitem_set.all()
         context["talks"] = self.object.talk_set.order_by("pk")
         return context
